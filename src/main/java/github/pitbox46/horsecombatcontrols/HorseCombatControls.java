beae8f2a709ae111f10840b893ca35a96c6470b0
pitbox46 <pitbox46@gmail.com>
2022-06-20 22:17:30 -0400

    Player retains combat mode on relog

--- src/main/java/github/pitbox46/horsecombatcontrols/HorseCombatControls.java
+++ src/main/java/github/pitbox46/horsecombatcontrols/HorseCombatControls.java
@@ -7,12 +7,14 @@ import github.pitbox46.horsecombatcontrols.network.PacketHandler;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -20,6 +22,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.PacketDistributor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -49,6 +52,12 @@ public class HorseCombatControls {
     private void onClientSetup(final FMLClientSetupEvent event) {
         toggleControls = new KeyMapping("key.horsecombatcontrols.toggle", 89, "key.horsecombatcontrols.category");
         ClientRegistry.registerKeyBinding(toggleControls);
+    }
+
+    @SubscribeEvent
+    public void onPlayerLogIn(PlayerEvent.PlayerLoggedInEvent event) {
+        if(!(event.getPlayer() instanceof ServerPlayer player)) return;
+        PacketHandler.CHANNEL.send(PacketDistributor.PLAYER.with(() -> player), new CombatModePacket(isInCombatMode(player)));
     }
 
     @OnlyIn(Dist.CLIENT)
