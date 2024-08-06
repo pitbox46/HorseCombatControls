5a97ee55fb11348428e3e8d27d9b4825e9ee9774
pitbox46 <pitbox46@gmail.com>
2024-08-06 13:50:54 -0400

    Fabric port

---
+++ fabric/src/main/java/github/pitbox46/horsecombatcontrols/network/CombatModePacket.java
@@ -0,0 +1,8 @@
+package github.pitbox46.horsecombatcontrols.network;
+
+import github.pitbox46.horsecombatcontrols.HorseCombatControls;
+import net.minecraft.resources.ResourceLocation;
+
+public class CombatModePacket {
+    public static final ResourceLocation ID = new ResourceLocation(HorseCombatControls.MODID, "combatmode");
+}
